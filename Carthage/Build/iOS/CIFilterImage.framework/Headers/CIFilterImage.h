defcef5032e82728e5b1647ff1e0721ab1b1f1b8
Tsubasa Hayashi <yoku.rin.99@gmail.com>
2019-03-29 19:18:46 +0900

    Support Carthage

---
+++ Carthage/Build/iOS/CIFilterImage.framework/Headers/CIFilterImage.h
@@ -0,0 +1,19 @@
+//
+//  CIFilterImage.h
+//  CIFilterImage
+//
+//  Created by Tsubasa Hayashi on 2019/03/29.
+//  Copyright © 2019 Tsubasa Hayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CIFilterImage.
+FOUNDATION_EXPORT double CIFilterImageVersionNumber;
+
+//! Project version string for CIFilterImage.
+FOUNDATION_EXPORT const unsigned char CIFilterImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CIFilterImage/PublicHeader.h>
+
+
